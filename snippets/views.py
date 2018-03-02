3571ff40221231ad4b44d2089b40a6f4a27d877a
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-03-02 19:05:45 +0500

    Authentication and Permission has been done

--- snippets/views.py
+++ snippets/views.py
@@ -11,6 +11,7 @@ from rest_framework import mixins
 from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 
 class UserList(generics.ListAPIView):
@@ -32,6 +33,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                      IsOwnerOrReadOnly,)
+    
 
 
