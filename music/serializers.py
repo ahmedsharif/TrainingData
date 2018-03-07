5e653870de7e6baedf74f19c90e2a9d5b3b91af7
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-03-07 14:57:40 +0500

    Django songs template error has been fixed.

---
+++ music/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Album, Song
+
+
+class AlbumSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Album
+        fields = ['id','user','artist','album_title','genre','album_logo','is_favorite']
+
+
+class SongSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['album','song_title','audio_file','is_favorite']
+
