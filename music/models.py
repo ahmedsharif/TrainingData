6cea85221bf5a849e7a9067e4a83c8ff26f09f4e
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-03-07 20:24:59 +0500

    Custom validation has been applied to application

--- music/models.py
+++ music/models.py
@@ -1,63 +1,41 @@
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 from django.db import models
-from django.core.validators import RegexValidator 
+
+text_validator = RegexValidator(
+    regex='^[a-zA-z0-9]*$',
+    message='title must be alphanumeric',
+    code='invalid Username'
+)
 
 
 class Album(models.Model):
-    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE,validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
+    user = models.ForeignKey(User, default=1, on_delete=models.CASCADE)
+    artist = models.CharField(max_length=250, validators=[
+        text_validator
     ])
-    artist = models.CharField(max_length=250,validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
+    album_title = models.CharField(max_length=500, validators=[
+        text_validator
     ])
-    album_title = models.CharField(max_length=500,validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
-    ])
-    genre = models.CharField(max_length=100,validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
+    genre = models.CharField(max_length=100, validators=[
+        text_validator
     ])
     album_logo = models.FileField()
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
-    
-    
+
+
 class Song(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE,validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, validators=[
+        text_validator
     ])
     song_title = models.CharField(max_length=250, validators=[
-        RegexValidator(
-            regex= '^[a-zA-z0-9]*$',
-            message='title must be alphanumeric',
-            code='invalid Username'
-        )
+        text_validator
     ])
     audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.song_title
-
-
--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+
+from . import views
 
 app_name = 'music'
 
