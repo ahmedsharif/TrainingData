0173809af0869ba1b5d2082a8737f629bd26ad85
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-03-08 17:48:26 +0500

    Signals has been applied to app

--- music/signals.py
+++ music/signals.py
@@ -1,13 +1,25 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from django.utils import timezone
 
-from website.models import Album
+from .models import Album
 
 @receiver(post_save, sender=Album)
 
-def album_list(sender,instance,**kwargs):
-    print ("hello ahmed%s",instance.album_title)
+def create_album_signal(sender,instance,created, **kwargs):
+    if created:
+        # instance.objects.create()
+        #print ("instance has been created successfully")
+        print ("%s is album id | %s is artist name | %s is album insertion time"
+        %(instance.id,instance.artist,timezone.now()))
     # instance.log += 'Task: %s | user: %s | Time:%s \n'%(
     #     instance.id, instance.artist, timezone.now()
     # )
+
+@receiver(post_delete, sender=Album)
+
+def delete_album_signal(sender,instance, *args, **kwargs):
+    if instance:
+        print ("%s is album id | %s is artist name | %s is album insertion time is deleted"
+        %(instance.id,instance.artist,timezone.now()))
+
