5e58fdc55eb2d54198336b63cd9772b2d93af744
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-04-10 19:24:29 +0500

    Curring and dispatch function has been applied.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,85 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { addRecipe, removeFromCalendar } from "./actions";
+
 
 class App extends Component {
+  //   state = {
+  //       calendar: null
+  //   }
+
+  //   componentDidMount() {
+  //     const {store} = this.props
+
+  //     store.subscribe(() => {
+  //       this.setState(() => ({
+  //         calendar: store.getState()
+  //       }))
+  //     })
+  //   }
+
+  // submitFood = () => {
+  //   this.props.store.dispatch(addRecipe({
+  //     day: 'monday',
+  //     meal: 'breakfast',
+  //     recipe: {
+  //       label: this.input.value
+  //     }
+  //   }))
+
+  //   this.input.value = ''
+  // }
+
   render() {
+    // doThing = () => {
+    //   this.props.selectRecipe({})
+    //   };
+
+    console.log("props", this.props);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        Hello World
+        {/*       
+      <input type="text" ref={(input) => this.input = input }
+      placeholder="Monday's Breakfast" />
+      <button onClick={this.submitFood}> Submit </button>
+
+      <pre>
+        Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast }
+      </pre>
+       */}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = [
+    "sunday",
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday"
+  ];
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : null;
+        return meals;
+      }, {})
+    }))
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -5,13 +5,17 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore} from 'redux'
 import reducer from './reducers'
-//import { Provider } from 'react-redux'
+import { Provider, connect } from 'react-redux'
 
 
 
-//const store = createStore(reducer)
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-// console.log(store)
+console.log(store)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
