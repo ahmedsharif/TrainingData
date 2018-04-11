0cb78d1c19f982b3ab18a9677ec7a3c46b5199fe
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-04-11 17:03:08 +0500

    Modal error has been resolved.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,57 @@ import { connect } from 'react-redux';
 import { addRecipe, removeFromCalendar } from './actions';
 import { captialize } from './utils/helper';
 import CalendarIcon from '../node_modules/react-icons/lib/fa/calendar-plus-o';
+import Modal from 'react-modal'
+import ArrowRightIcon from '../node_modules/react-icons/lib/fa/arrow-circle-right';
+import Loading from 'react-loading';
+import { fetchRecipes } from './utils/api';
+import  FoodList  from './components/FoodList';
 
 class App extends Component {
+  state = {
+    foodModalOpen: false,
+    meal: null,
+    day: null,
+    food: null,
+    loadingFood: false,
+  };
+
+  openFoodModel = ({ meal, day }) => {
+    this.setState(() => ({
+      foodModalOpen: true,
+      meal,
+      day,
+    }));
+  };
+
+  closeFoodModel = () => {
+    this.setState(() => ({
+      foodModalOpen: false,
+      meal: null,
+      day: null,
+      food: null,
+    }));
+  };
+
+  searchFood = e => {
+    if (!this.input.value) {
+      return;
+    }
+
+    e.preventDefault();
+
+    this.setState(() => ({ loadingFood: true }));
+    fetchRecipes(this.input.value).then(food =>
+      this.setState(() => ({
+        food,
+        loadingFood: false,
+      }))
+    );
+  };
+
   render() {
-    const { calendar, remove } = this.props;
+    const { foodModalOpen, loadingFood, food } = this.state;
+    const { calendar, remove, selectRecipe } = this.props;
     const mealOrder = ['breakfast', 'lunch', 'dinner'];
 
     return (
@@ -41,7 +88,10 @@ class App extends Component {
                         </button>
                       </div>
                     ) : (
-                      <button className="icon-btn">
+                      <button
+                        onClick={() => this.openFoodModel({ meal, day })}
+                        className="icon-btn"
+                      >
                         <CalendarIcon size={30} />
                       </button>
                     )}
@@ -51,6 +101,54 @@ class App extends Component {
             ))}
           </div>
         </div>
+        <Modal
+          className="modal"
+          overlayClassName="overlay"
+          isOpen={foodModalOpen}
+          onRequestClose={this.closeFoodModal}
+          contentLabel="Modal"
+        >
+          <div>
+            {loadingFood === true ? (
+              <Loading
+                delay={200}
+                type="spin"
+                color="#222"
+                className="loading"
+              />
+            ) : (
+              <div className="search-container">
+                <h3 className="subheader">
+                  Find a meal for {captialize(this.state.day)} {this.state.meal}.
+                </h3>
+                <div className="search">
+                  <input
+                    className="food-input"
+                    type="text"
+                    placeholder="Search Foods"
+                    ref={input => (this.input = input)}
+                  />
+                  <button className="icon-btn" onClick={this.searchFood}>
+                    <ArrowRightIcon size={30} />
+                  </button>
+                </div>
+                {food !== null && (
+                  <FoodList
+                    food={food}
+                    onSelect={recipe => {
+                      selectRecipe({
+                        recipe,
+                        day: this.state.day,
+                        meal: this.state.meal,
+                      });
+                      this.closeFoodModal();
+                    }}
+                  />
+                )}
+              </div>
+            )}
+          </div>
+        </Modal>
       </div>
     );
   }
