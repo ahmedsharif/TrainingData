0aa5c8a36eac9ea356935f65325c75a05b1de89f
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-04-11 15:52:13 +0500

    Normalization has been applied to Redux store

--- src/App.js
+++ src/App.js
@@ -1,68 +1,70 @@
-import React, { Component } from "react";
-import "./App.css";
-import { connect } from "react-redux";
-import { addRecipe, removeFromCalendar } from "./actions";
-
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from './actions';
+import { captialize } from './utils/helper';
+import CalendarIcon from '../node_modules/react-icons/lib/fa/calendar-plus-o';
 
 class App extends Component {
-  //   state = {
-  //       calendar: null
-  //   }
-
-  //   componentDidMount() {
-  //     const {store} = this.props
-
-  //     store.subscribe(() => {
-  //       this.setState(() => ({
-  //         calendar: store.getState()
-  //       }))
-  //     })
-  //   }
-
-  // submitFood = () => {
-  //   this.props.store.dispatch(addRecipe({
-  //     day: 'monday',
-  //     meal: 'breakfast',
-  //     recipe: {
-  //       label: this.input.value
-  //     }
-  //   }))
-
-  //   this.input.value = ''
-  // }
-
   render() {
-    // doThing = () => {
-    //   this.props.selectRecipe({})
-    //   };
+    const { calendar, remove } = this.props;
+    const mealOrder = ['breakfast', 'lunch', 'dinner'];
 
-    console.log("props", this.props);
     return (
-      <div>
-        Hello World
-        {/*       
-      <input type="text" ref={(input) => this.input = input }
-      placeholder="Monday's Breakfast" />
-      <button onClick={this.submitFood}> Submit </button>
+      <div className="container">
+        <ul className="meal-types">
+          {mealOrder.map(mealType => (
+            <li key={mealType} className="subheader">
+              {captialize(mealType)}
+            </li>
+          ))}
+        </ul>
 
-      <pre>
-        Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast }
-      </pre>
-       */}
+        <div className="calendar">
+          <div className="days">
+            {calendar.map(({ day }) => (
+              <h3 key={day} className="subheader">
+                {captialize(day)}
+              </h3>
+            ))}
+          </div>
+          <div className="icon-grid">
+            {calendar.map(({ day, meals }) => (
+              <ul key={day}>
+                {mealOrder.map(meal => (
+                  <li key={meal} className="meal">
+                    {meals[meal] ? (
+                      <div className="food-item">
+                        <img src={meals[meal].image} alt={meals[meal].label} />
+                        <button onClick={() => remove({ meal, day })}>
+                          Clear
+                        </button>
+                      </div>
+                    ) : (
+                      <button className="icon-btn">
+                        <CalendarIcon size={30} />
+                      </button>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({calendar, food} ) {
+function mapStateToProps({ calendar, food }) {
   const dayOrder = [
-    "sunday",
-    "monday", 
-    "tuesday",
-    "wednesday",
-    "thursday",
-    "friday",
-    "saturday"
+    'sunday',
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
   ];
   return {
     calendar: dayOrder.map(day => ({
@@ -70,15 +72,15 @@ function mapStateToProps({calendar, food} ) {
       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
         meals[meal] = food[calendar[day][meal]] ? calendar[day][meal] : null;
         return meals;
-      }, {})
-    }))
+      }, {}),
+    })),
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    selectRecipe: (data) => dispatch(addRecipe(data)),
-    remove: (data) => dispatch(removeFromCalendar(data))
+    selectRecipe: data => dispatch(addRecipe(data)),
+    remove: data => dispatch(removeFromCalendar(data)),
   };
 }
 
---
+++ src/utils/helper.js
@@ -0,0 +1,5 @@
+export function captialize (str='') {
+    return typeof str !== 'string'
+    ? ''
+    : str[0].toUpperCase() + str.slice(1)
+}
